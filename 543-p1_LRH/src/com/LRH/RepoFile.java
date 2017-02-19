792f0d0864d94330514826ec42e8275ba47bae4d
tlind15 <tlind15@gmail.com>
2017-02-19 01:01:38 -0800

    Make helper functions private

--- 543-p1_LRH/src/com/LRH/RepoFile.java
+++ 543-p1_LRH/src/com/LRH/RepoFile.java
@@ -12,12 +12,12 @@ public class RepoFile extends File {
         super(path); //is the default constructor for the java 'File' class
     }
 
-    public void copyFolder(String dest) throws IOException {
+    private void copyFolder(String dest) throws IOException {
         File file_dest = FileUtils.getFile(dest + "\\" + this.getName()); //creates file path for new destination from String
         FileUtils.copyDirectory(this, file_dest);
     }
     
-    public void createLeafFolder (File file) throws IOException {
+    private void createLeafFolder (File file) throws IOException {
         final String TEMP_DIR = "scm_temp";
         File tempDir = FileUtils.getFile(file.getParent() + "\\" + TEMP_DIR);
         FileUtils.moveFileToDirectory(file, tempDir, true);
