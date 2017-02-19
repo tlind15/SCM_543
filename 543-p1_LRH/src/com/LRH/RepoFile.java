fad6c24b31bb9c266ea89d7ce4aebfe8adb0b126
tlind15 <tlind15@gmail.com>
2017-02-18 23:47:41 -0800

    Add copyFolder function
    This function simply copies the content of one folder into another folder. The copy has the same name as the original folder.

--- 543-p1_LRH/src/com/LRH/RepoFile.java
+++ 543-p1_LRH/src/com/LRH/RepoFile.java
@@ -1,14 +1,19 @@
 package com.LRH;
 
+import org.apache.commons.io.FileUtils;
+
 import java.io.File;
+import java.io.IOException;
 
 
 public class RepoFile extends File {
-
-    private String path;
-
+    
     public RepoFile (String path) {
         super(path); //is the default constructor for the java 'File' class
-        this.path = path;
+    }
+
+    public void copyFolder(String dest) throws IOException {
+        File file_dest = FileUtils.getFile(dest + "\\" + this.getName()); //creates file path for new destination from String
+        FileUtils.copyDirectory(this, file_dest);
     }
 }
