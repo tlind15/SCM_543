2610f3487955f4d046f7d425cf3641d06cd568b8
tlind15 <tlind15@gmail.com>
2017-02-26 17:14:42 -0800

    Add basic running code

--- 543-p1_LRH/src/com/LRH/Main.java
+++ 543-p1_LRH/src/com/LRH/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	    //create GUI
+	    RepoFile rf = new RepoFile("C:\\Users\\tlindblom\\Downloads\\test_repo", "user");
+	    rf.createRepo("C:\\Users\\tlindblom\\Documents\\repository");
     }
 }
