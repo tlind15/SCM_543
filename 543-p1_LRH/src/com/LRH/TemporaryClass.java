73ed44ceacb3ae73ec5590e5e4c84936a5132d4b
Jocelyn Ramirez <jsyramirez@gmail.com>
2017-05-04 23:27:47 -0700

    TemporaryClass.java
    Contains the manifest file functions and parser.

---
+++ 543-p1_LRH/src/com/LRH/TemporaryClass.java
@@ -0,0 +1,92 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.PrintWriter;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Timestamp;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
+
+
+public class TemporaryClass
+{
+	//Returns a list of all added files in the manifest
+	//Each row follows this indexing
+	//0: file_name	
+	//1: path_in_repo
+	//2: artifact id	
+	//3: File Location
+	//4: Version
+	public List<String []> parseManifestFile() {
+		String file = getManifestFile().getAbsolutePath();
+        List<String[]> filesList = new ArrayList<String[]>();
+
+        try(FileReader fileReader = new FileReader(file))
+        {
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            StringBuffer stringBuffer = new StringBuffer();
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+            	if(line.toLowerCase().contains("adding file...")) {
+            		System.out.println("Test");
+ 					String[] spt = line.split("\t");
+ 					filesList.add(new String[] {spt[2], spt[4], spt[6], spt[8], spt[10]});
+            	}
+            }
+            fileReader.close();
+    		for (String[] row : filesList) {
+        		System.out.println("Row = " + Arrays.toString(row));
+    		}
+    		return filesList;
+        }catch (IOException f) {System.out.println("Could not read manifest file");}
+        return filesList;
+    }
+	public void writeToManifesto_RepoCreate(String repo_loc, String dir_cpy, String username, String manifestoFile)
+	{
+		Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+		try (PrintWriter out = new PrintWriter(new BufferedWriter(new 
+									OutputStreamWriter(new FileOutputStream(manifestoFile, true), StandardCharsets.UTF_8)))) {
+    		out.println("User:\t" + username + "\tCreated Repo at:\t" + repo_loc + "\t" + "Version:\t1");
+		} catch (IOException e) {System.out.println("Could not write to Activity Logs");}
+	    //exception handling left as an exercise for the reader
+	}
+	//check-out repofoldername emptyfolder version
+    private void writeToManifesto_CheckOut(String repoFolderName, String checkOutFolder, String version)
+    {
+        try (PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(new FileOutputStream(getManifestFile().getAbsolutePath(), true), StandardCharsets.UTF_8)))) {
+            out.println("Check-out from\tRepo Folder:\t" + repoFolderName + "\tto Folder:\t" + checkOutFolder
+            		+ "\tVersion:\t" + version);
+        } catch (IOException e) {System.out.println("Could not write checked out file");}
+        //exception handling left as an exercise for the reader
+    }
+    //check-in repofoldername checkoutfolder
+    private void writeToManifesto_CheckIn(String repoFolderName, String checkOutFolder)
+    {
+        try (PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(new FileOutputStream(getManifestFile().getAbsolutePath(), true), StandardCharsets.UTF_8)))) {
+        	String version = getNewVersion();
+            out.println("Check-In to\tRepo Folder:\t" + repoFolderName + "\tfrom Folder:\t" + checkOutFolder
+            		+ "\tVersion:\t" + version);
+        } catch (IOException e) {System.out.println("Could not write checked out file");}
+        //exception handling left as an exercise for the reader
+    }
+    private void writeToManifesto_FileAdded(File artifactFile, String original_file_name, String path_in_repo, String version)
+    {
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        try (PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(new FileOutputStream(getManifestFile().getAbsolutePath(), true), StandardCharsets.UTF_8)))) {
+            out.println("Adding File...\tFile Name:\t" + original_file_name + "\tPath In Repo\t" + path_in_repo + "\tArtifact ID:\t" + 
+            		artifactFile.getName().substring(0, artifactFile.getName().lastIndexOf(".")) + "\tFile Location:\t" + artifactFile.getAbsolutePath()
+            		+ "\tVersion:\t" + version);
+        } catch (IOException e) {System.out.println("Could not write added file");}
+        //exception handling left as an exercise for the reader
+    }
+    public static void main(String args[])
+    {
+        TemporaryClass obj = new TemporaryClass();
+        obj.parseManifestFile();
+    }
+}
